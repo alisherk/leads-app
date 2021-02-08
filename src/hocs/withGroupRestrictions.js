37420ffe4f363ac019a390fb00b8185ec5d48bac
Alisher K <alisherkabildjanov@yahoo.com>
2021-02-08 16:32:10 -0600

    fixed user model

--- src/hocs/withGroupRestrictions.js
+++ src/hocs/withGroupRestrictions.js
@@ -1,8 +1,6 @@
 import React from 'react';
-
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-
 import RestrictToGroups from 'components/RestrictToGroups';
 
 function FallbackComponent () {
